7986838932bc31c5e73ecf9ac533f97b17d24936
0wan1001 <rotk125@naver.com>
2026-08-27 19:38:12 +0900

    [Feat] 0wan1001 자기소개 메서드 추가 및 소개 출력 기능 구현
    Closes #1

--- mp15-260827-6조/Program.cs
+++ mp15-260827-6조/Program.cs
@@ -5,10 +5,13 @@ public class Program
     public static void Main()  
     {  
         Console.WriteLine("=== 6조 팀원 소개 ===");  
-        // 여기에 팀원 소개 호출을 한 줄씩 추가합니다
+        _0wan1001();
 
         Console.WriteLine("=== 소개 끝 ===");  
     }
-
-    // 여기에 팀원 소개 메서드를 하나씩 추가합니다  
+    
+    public static void _0wan1001()
+    {
+        Console.WriteLine("장영환 / 가위바위보 져서 팀장을 맡았습니다");
+    }
 }
